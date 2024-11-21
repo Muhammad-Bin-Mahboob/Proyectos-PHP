b03ec4c0adc989a89441b6a1e464c94e863ecd9a
Muhammad Bin Mahboob <muhammadbinm123@gmail.com>
2024-11-21 20:44:48 +0100

    Add files via upload

---
+++ DiscografiaMuhammad/includes/connection.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Discografía
+*
+* @author Muhammad
+* @version 2.0
+*/
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=discografia';
+$user = 'vetustamorla';
+$pass = '15151';
+$options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
